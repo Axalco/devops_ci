c6dfff5fe4fa6b130f992cd4d14b66f810d47caf
Axalco <38451781+Axalco@users.noreply.github.com>
2022-02-23 14:48:24 -0600

    prueba webhooks cambio en test v2

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -1,7 +1,9 @@
 package com.paymentchain.billing;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.postgresql.translation.messages_bg;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,7 +13,7 @@ public class BasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		String message="default mensaje cambio test devops";
+		String message= "default mensaje cambio test devops";
 		Assert.assertNotNull(message);
 	}
 
